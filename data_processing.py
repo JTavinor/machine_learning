33c2a9edbcbe26910538eae2b40ad01561b07a9f
Joe Tavinor <joe.tavinor@appscore.net>
2023-12-12 22:17:31 +0000

    import the data set

--- data_processing.py
+++ data_processing.py
@@ -1,3 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+dataset = pd.read_csv('data/Data.csv')
+x = dataset.iloc[:, :-1].values # Matrix of features/independent variables
+y = dataset.iloc[:, -1].values # Dependant variable vector
+print(x)
+print(y)
