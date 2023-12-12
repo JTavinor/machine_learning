c61e038243397f9bc158ab092e8c8b822c2baa6e
Joe Tavinor <joe.tavinor@appscore.net>
2023-12-12 23:13:18 +0000

    fill in missing data with the imputer

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
+
 
 dataset = pd.read_csv('data/Data.csv')
 x = dataset.iloc[:, :-1].values # Matrix of features/independent variables
 y = dataset.iloc[:, -1].values # Dependant variable vector
+print(x, y)
+
+# For missing data we can either remove the row, if the rows with missing data is a small percentage of the data set
+# Or we can replace the missing data with the average of the column
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+print(x[:, 1:])
+
+x[:, 1:] = imputer.fit(x[:, 1:]).transform(x[:, 1:])
+
 print(x)
-print(y)
