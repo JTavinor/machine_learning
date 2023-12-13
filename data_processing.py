67ba9ec757c93b40d4c4668d98c8583293dd6835
Joe Tavinor <joe.tavinor@appscore.net>
2023-12-13 13:29:03 +0000

    encode dependant variable vector

--- data_processing.py
+++ data_processing.py
@@ -4,24 +4,32 @@ import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 
 dataset = pd.read_csv('data/Data.csv')
-x = dataset.iloc[:, :-1].values # Matrix of features/independent variables
+X = dataset.iloc[:, :-1].values # Matrix of features/independent variables
 y = dataset.iloc[:, -1].values # Dependant variable vector
-print(x, y)
+print(X, y)
 
 # For missing data we can either remove the row, if the rows with missing data is a small percentage of the data set
 # Or we can replace the missing data with the average of the column
 imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
-print(x[:, 1:])
+print(X[:, 1:])
 
-x[:, 1:] = imputer.fit(x[:, 1:]).transform(x[:, 1:])
+X[:, 1:] = imputer.fit(X[:, 1:]).transform(X[:, 1:])
 
-print(x)
+print(X)
 
 # Use One-hot encoding for categorical (non-numerical) information - if we covert to numbers, the MLM may infer an order
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
-x = np.array(ct.fit_transform(x))
+X = np.array(ct.fit_transform(X))
 
-print(x)
+print(X)
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+print(y)
+
+
